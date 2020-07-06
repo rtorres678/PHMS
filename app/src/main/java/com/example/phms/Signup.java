fd280dcafcf92762f39c971b852ef360e6ef5a19
xhfhope <logan.plymale@mavs.uta.edu>
2020-07-06 11:21:51 -0500

    Login / Signup / Firebase / Theme
    Implements several activities, organizes and integrates firebase into the project files, adds user authentication via firebase

---
+++ app/src/main/java/com/example/phms/Signup.java
@@ -0,0 +1,141 @@
+package com.example.phms;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthMultiFactorException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.MultiFactorResolver;
+
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class Signup extends AppCompatActivity {
+
+    //============================================
+    // DECLARE JAVA VARIABLES AND FIREBASE USAGE FOR THIS FORM
+    //============================================
+    TextInputLayout regEmailAddress, regPassword;
+    Button regSignupBtn, regGoToLoginBtn;
+    private FirebaseAuth mAuth;
+
+    private static final String TAG = "EmailPassword";
+
+    //============================================
+    // HAVE TO MAKE AN OBJECT TO ACCESS A LOGIN_SUCCESSFUL BOOLEAN GLOBALLY
+    //============================================
+
+
+    //============================================
+    // ONCREATE() -> ON ACTIVITY LOAD
+    //============================================
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //This Line will hide the status bar from the screen
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().hide();
+        mAuth = FirebaseAuth.getInstance();
+
+        //============================================
+        // CREATE AND RELATE JAVA VARIABLES WITH EACH XML ELEMENT
+        //============================================
+        regEmailAddress = findViewById(R.id.reg_emailAddress);
+        regPassword = findViewById(R.id.reg_password);
+        regSignupBtn = findViewById(R.id.reg_signupBtn);
+        regGoToLoginBtn = findViewById(R.id.reg_goToLoginBtn);
+
+        //============================================
+        // SIGNUP BUTTON LISTENER
+        //============================================
+        regSignupBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //============================================
+                // Could use some form validation!
+                // Fine for now -> Proceed to register input in database
+                //============================================
+                //Get all the values
+                String emailAddress = regEmailAddress.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+                createAccount(emailAddress, password);
+                if(getSignupSuccessful()) {
+                    Log.d(TAG, "signInWithEmail:success");
+                    signIn(emailAddress, password);
+                    Intent intent = new Intent(view.getContext(), BasicInfo.class);
+                    view.getContext().startActivity(intent);
+                }
+                }
+                //============================================
+                // Now load the signup_cont Activity
+                //============================================
+                //Intent intent = new Intent(view.getContext(), Signup.class);
+                //view.getContext().startActivity(intent);
+        });
+
+        //============================================
+        // GO TO LOGIN PAGE BUTTON LISTENER
+        //============================================
+        regGoToLoginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Login.class);
+                view.getContext().startActivity(intent);}
+        });
+    }
+
+    public void onStart(){
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //success, update UI with signed-in user info
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(Signup.this, "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                            setSignupSuccessful(true);
+                        } else {
+                            // sign in fails, dispaly fail message
+                            Toast.makeText(Signup.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void signIn(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            Toast.makeText(Signup.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/phms/User.java
@@ -0,0 +1,83 @@
+package com.example.phms;
+
+import java.util.Date;
+
+public class User {
+
+    String name, username, email, password, phoneNumber;
+    int gender, weight, height, calorieGoal;
+    Date checkupDate;
+
+    public User() {}
+
+    public User(String name, String email, String password, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getCalorieGoal() {
+        return calorieGoal;
+    }
+
+    public void setCalorieGoal(int calorieGoal) {
+        this.calorieGoal = calorieGoal;
+    }
+}
