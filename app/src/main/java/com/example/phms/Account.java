5041a9e61364e7bf69fbf9f4323e2b50b8dbf8e0
xhfhope <logan.plymale@mavs.uta.edu>
2020-08-13 07:29:19 -0500

    weight, food, icon
    functioning browsing

--- app/src/main/java/com/example/phms/Account.java
+++ app/src/main/java/com/example/phms/Account.java
@@ -19,8 +19,13 @@ import com.google.firebase.database.ValueEventListener;
 
 
 public class Account extends AppCompatActivity {
+<<<<<<< Updated upstream
     Button btnSampleButton, btnSampleButton2, btnSampleButton3, btnSampleButton4;
     TextView headerText;
+=======
+    Button regUpdateBtn, btnCancel;
+    TextInputLayout regName, regPhoneNumber, regGender, regWeight, regHeight, regCalorieGoal;
+>>>>>>> Stashed changes
     DatabaseReference db_ref;
     private static final String TAG = "Account";
 
@@ -33,7 +38,69 @@ public class Account extends AppCompatActivity {
         //============================================
         // VARIABLES
         //============================================
+<<<<<<< Updated upstream
         btnSampleButton = (Button)findViewById(R.id.btnSampleButton);
         headerText = (TextView)findViewById(R.id.headerText);
+=======
+        //============================================
+        // Grab fields from XML
+        //============================================
+        regName = findViewById(R.id.reg_name);
+        regPhoneNumber = findViewById(R.id.reg_phoneNumber);
+        regGender = findViewById(R.id.reg_gender);
+        regWeight = findViewById(R.id.reg_weight);
+        regHeight = findViewById(R.id.reg_height);
+        regCalorieGoal = findViewById(R.id.reg_calorieGoal);
+        regUpdateBtn = findViewById(R.id.reg_updateBtn);
+        btnCancel = findViewById(R.id.btnCancel);
+        //============================================
+        // USER CLASS INSTANTIATION, DB INITIALIZATION
+        //============================================
+        user = new User();
+        db_ref = FirebaseDatabase.getInstance().getReference().child("Users");
+        //============================================
+        // WHEN USER CLICKS "NEXT"
+        //============================================
+        regUpdateBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //============================================
+                // Could use some form validation!
+                // Fine for now -> Proceed to register input in database
+                //============================================
+                //Get all the values
+                String name = regName.getEditText().getText().toString();
+                String phoneNumber = regPhoneNumber.getEditText().getText().toString();
+                String gender = regGender.getEditText().getText().toString();
+                int weight = Integer.parseInt(regWeight.getEditText().getText().toString());
+                int height = Integer.parseInt(regHeight.getEditText().getText().toString());
+                int calorieGoal = Integer.parseInt(regCalorieGoal.getEditText().getText().toString());
+                user.setName(name);
+                user.setPhoneNumber(phoneNumber);
+                user.setGender(gender);
+                user.setWeight(weight);
+                user.setHeight(height);
+                user.setCalorieGoal(calorieGoal);
+
+                //============================================
+                // SPECIFY NAME OF CHILD NODE OF USER CLASS THAT WE ARE GOING TO ADD
+                //============================================
+                db_ref.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user);
+                //upload to db
+                Log.d(TAG, "DB Update executed");
+
+                Intent intent = new Intent(view.getContext(), Home.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), Home.class);
+                startActivity(intent);
+            }
+        });
+>>>>>>> Stashed changes
     }
 }
--- app/src/main/java/com/example/phms/AddWeight.java
+++ app/src/main/java/com/example/phms/AddWeight.java
@@ -1,6 +1,5 @@
 package com.example.phms;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,30 +12,26 @@ import java.util.Locale;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-public class AddFood extends AppCompatActivity {
+public class AddWeight extends AppCompatActivity {
 
     Button btnSave, btnCancel;
-    TextInputLayout foodName, foodCount;
+    TextInputLayout weight;
     DatabaseReference db_ref;
-    private static final String TAG = "AddFood";
-    Food food;
-    long maxid;
+    private static final String TAG = "AddWeight";
+    int iWeight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_food);
+        setContentView(R.layout.activity_add_weight);
         getSupportActionBar().hide();
         //============================================
         // Grab fields from XML
         //============================================
-        foodName = findViewById(R.id.foodName);
-        foodCount = findViewById(R.id.foodCount);
+        weight= findViewById(R.id.weight);
+
 
         btnSave = (Button)findViewById(R.id.btnSave);
         btnCancel = (Button)findViewById(R.id.btnCancel);
@@ -45,32 +40,17 @@ public class AddFood extends AppCompatActivity {
         //============================================
         SimpleDateFormat df = new SimpleDateFormat("yyyy-M-d", Locale.US);
         String time = df.format(new Date());
-        Log.d(TAG, "phms_time: "+time);
-
 
         String uid =FirebaseAuth.getInstance().getCurrentUser().getUid(); //get current user's id
-        db_ref = FirebaseDatabase.getInstance().getReference().child("Users").child(uid).child("Food").child(time); //refer to /Users/<uid>/Notes
-        db_ref.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists())
-                    maxid=snapshot.getChildrenCount(); //make a snapshot, grab the "children count" which means the number of children of this users Notes (# of notes)
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        food = new Food();
+        db_ref = FirebaseDatabase.getInstance().getReference().child("Users").child(uid).child("Weights").child(time);
 
         btnSave.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                food.setname(foodName.getEditText().getText().toString());
-                food.setcalories(foodCount.getEditText().getText().toString());
-                db_ref.child(String.valueOf(maxid+1)).setValue(food);
-
+                iWeight = Integer.parseInt(weight.getEditText().getText().toString());
+                db_ref.setValue(iWeight);
+                db_ref = FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("weight");
+                db_ref.setValue(iWeight);
                 Intent intent = new Intent(view.getContext(), Health.class);
                 startActivity(intent);
             }
